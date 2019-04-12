c599e1ba4ccddb2ae8403f64a922ecd851b04b97
Konstantin Kaluzhnikov <evilkraft@gmail.com>
2019-04-12 10:44:07 +0400

    fix cli-test

--- cli-test.php
+++ cli-test.php
@@ -4,7 +4,7 @@
 define('FIRSTKEY', 'nEZT13dW7Fzi68IV48KVIAv+4ECJcHwQE4HFG6Dz1ZE=');
 define('SECONDKEY','ouj9xfTewG31YAk2lEkRkJTIYzxlhGXY9GaFGMAaL61QT5euOwWhAce+v92fGOLDIh+HhFHXLcXl5LKZbmgpfg==');
 
-require_once('./src/EncDec.php');
+require_once(__DIR__.'/src/EncDec.php');
 
 $filename_in  = $argv[1];
 $filename_out = $argv[2];
