c3be183dee46131d0627d9575f676fe4ed67da3f
Konstantin Kaluzhnikov <evilkraft@gmail.com>
2019-04-12 10:12:30 +0400

    add cli-test

---
+++ cli-test.php
@@ -0,0 +1,12 @@
+<?php
+
+// Save The Keys In Your Configuration File
+define('FIRSTKEY', 'nEZT13dW7Fzi68IV48KVIAv+4ECJcHwQE4HFG6Dz1ZE=');
+define('SECONDKEY','ouj9xfTewG31YAk2lEkRkJTIYzxlhGXY9GaFGMAaL61QT5euOwWhAce+v92fGOLDIh+HhFHXLcXl5LKZbmgpfg==');
+
+require_once('./src/EncDec.php');
+
+$filename_in  = $argv[1];
+$filename_out = $argv[2];
+
+\EvilKraft\encdec\EncDec::obfus($filename_in, $filename_out);
